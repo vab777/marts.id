06245faf05ce700d43af56927b82b9206bad1e98
vab777 <valentabadi@Valents-MacBook-Pro.local>
2015-08-19 14:15:10 +0700

    This is initial commit

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=middb',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+];
